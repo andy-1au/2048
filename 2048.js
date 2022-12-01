581bf416bf015adfae2c6406137c75fccec0f647
andy-1au <andyolau888@gmail.com>
2022-12-01 01:09:01 -0500

    final commit

--- 2048.js
+++ 2048.js
@@ -9,8 +9,8 @@ window.onload = function() {
 
 function newGame() {
     board = [
-        [2, 2, 0, 0],
-        [0, 16, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
@@ -23,6 +23,41 @@ function newGame() {
             let num = board[r][c]; 
             updateTile(tile, num);
             document.getElementById("board").append(tile);
+        }
+    }
+
+    generateTile(); // generate a "2" tile twice to start the game
+    generateTile();
+}
+
+function isFull() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (board[r][c] == 0) {
+                return false; // if there is a zero, the board is not full
+            }
+        }
+    } 
+    return true; //otherwise, the board is full
+}
+
+function generateTile() {
+    if (isFull()) {
+        return; // if the board is full, don't generate a tile
+    }
+
+    let found = false;
+
+    while (!found) {
+        // generate a random row and column value 
+        let r = Math.floor(Math.random() * rows); // 0 - 3
+        let c = Math.floor(Math.random() * cols);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString()); // update the tile, html element
+            let num = board[r][c]; // get the number of the tile from the board
+            updateTile(tile, num); 
+            found = true;
         }
     }
 }
@@ -43,12 +78,24 @@ function updateTile(tile, num) {
 }
 
 document.addEventListener("keyup", (e) => {
+    // Arrow Keys
     if (e.code == "ArrowLeft") {
         slideLeft();
+        generateTile(); // generate a new tile after sliding
     } 
     else if (e.code == "ArrowRight") {
         slideRight();
+        generateTile();
+    } 
+    else if (e.code == "ArrowUp") {
+        slideUp();
+        generateTile();
     }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+        generateTile();
+    }
+    document.getElementById("score").innerText = score;
 })
 
 function filterZero(row) {
@@ -95,10 +142,44 @@ function slideRight() {
         let row = board[r]; // get the row
         row.reverse(); // reverse the row, so we can slide left, this is the same as sliding right once we reverse it back
         row = slide(row); // slide the row
-        row.reverse(); 
+        row.reverse();  
         board[r] = row; // update the board with the new row
 
         for (let c = 0; c < cols; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c]; // get the number of the tile from the board
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideUp() {
+    for (let c = 0; c < cols; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]]; // get the column
+        row = slide(row); 
+        
+        for (let r = 0; r < rows; r++) {
+            // update the board with the new column
+            board[r][c] = row[r];
+
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c]; // get the number of the tile from the board
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideDown() {
+    for (let c = 0; c < cols; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]]; // get the column
+        row.reverse();
+        row = slide(row); 
+        row.reverse();
+        
+        for (let r = 0; r < rows; r++) {
+            // update the board with the new column
+            board[r][c] = row[r];
+            
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c]; // get the number of the tile from the board
             updateTile(tile, num);
