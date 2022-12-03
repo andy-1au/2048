9d7cdbe2d57b2c629ceebb81433639be1d44c89e
andy-1au <andyolau888@gmail.com>
2022-12-02 19:16:21 -0500

    for michael push

--- 2048.js
+++ 2048.js
@@ -115,8 +115,6 @@ function generateTile() {
 function updateTile(tile, num) {
     tile.innerText = ""; // clear the tile number
     tile.classList.value = ""; // clear the tile class
-    tile.classList.add("tile"); // add the tile class back
-
     if (num > 0) {
         tile.innerText = num; // set the tile number
         if (num <= 4096) {
@@ -125,6 +123,11 @@ function updateTile(tile, num) {
             tile.classList.add("x8192");
         }
     }
+
+    tile.classList.add("tile"); // add the tile class back
+    tile.classList.add("tileAnimation"); // add the tile animation class
+
+    
     document.getElementById("score").innerText = score; // update the score
 }
 
