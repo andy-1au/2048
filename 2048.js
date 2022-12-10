05ef3f8e8f5465335489a936189cf68db240bfc9
andy-1au <andyolau888@gmail.com>
2022-12-10 16:36:02 -0500

    removed comments in resetBoard()

--- 2048.js
+++ 2048.js
@@ -177,13 +177,6 @@ function newGame() {
             [0, 0, 0, 0]
         ]
 
-    /* 
-    [2, 4, 8, 16],
-    [32, 64, 128, 256],
-    [512, 1024, 0, 0],
-    [0, 0, 0, 0] 
-    */
-
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             let tile = document.createElement('div');
